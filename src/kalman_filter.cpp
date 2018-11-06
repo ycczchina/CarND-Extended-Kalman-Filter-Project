16961963d70aa3678f2e42330f58627ec277e747
ycczchina <chenzhen@umich.edu>
2018-11-05 23:12:25 -0500

    Submission

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -3,7 +3,7 @@
 using Eigen::MatrixXd;
 using Eigen::VectorXd;
 
-// Please note that the Eigen library does not initialize 
+// Please note that the Eigen library does not initialize
 // VectorXd or MatrixXd objects with zeros upon creation.
 
 KalmanFilter::KalmanFilter() {}
@@ -25,6 +25,9 @@ void KalmanFilter::Predict() {
   TODO:
     * predict the state
   */
+	x_ = F_ * x_;
+	MatrixXd Ft = F_.transpose();
+	P_ = F_ * P_ * Ft + Q_;
 }
 
 void KalmanFilter::Update(const VectorXd &z) {
@@ -32,11 +35,54 @@ void KalmanFilter::Update(const VectorXd &z) {
   TODO:
     * update the state by using Kalman Filter equations
   */
+  VectorXd z_pred = H_ * x_;
+	VectorXd y = z - z_pred;
+	MatrixXd Ht = H_.transpose();
+	MatrixXd S = H_ * P_ * Ht + R_;
+	MatrixXd Si = S.inverse();
+	MatrixXd PHt = P_ * Ht;
+	MatrixXd K = PHt * Si;
+
+	//new estimate
+	x_ = x_ + (K * y);
+	long x_size = x_.size();
+	MatrixXd I = MatrixXd::Identity(x_size, x_size);
+	P_ = (I - K * H_) * P_;
 }
+
+const double PI = std::atan(1.0)*4;
 
 void KalmanFilter::UpdateEKF(const VectorXd &z) {
   /**
   TODO:
     * update the state by using Extended Kalman Filter equations
   */
+  float px = x_(0);
+ 	float py = x_(1);
+ 	float vx = x_(2);
+ 	float vy = x_(3);
+  float a1 = px * px + py * py;
+  float a2 = sqrt(a1);
+  float a3 = px * vx  + py * vy;
+
+  VectorXd z_pred = VectorXd(3);
+  z_pred << a2, atan2(py, px), a3 / a2;
+  VectorXd y = z - z_pred;
+  while (y[1] > PI || y[1] < -PI) {
+    if (y[1] > PI)
+      y[1] -= 2 * PI;
+    else if (y[1] < -PI)
+      y[1] += 2 * PI;
+  }
+  MatrixXd Ht = H_.transpose();
+  MatrixXd S = H_ * P_ * Ht + R_;
+  MatrixXd Si = S.inverse();
+  MatrixXd PHt = P_ * Ht;
+  MatrixXd K = PHt * Si;
+
+  //new estimate
+  x_ = x_ + (K * y);
+  long x_size = x_.size();
+  MatrixXd I = MatrixXd::Identity(x_size, x_size);
+  P_ = (I - K * H_) * P_;
 }
